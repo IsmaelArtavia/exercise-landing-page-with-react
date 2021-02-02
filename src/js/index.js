6edb1a0f52914a9642ba835e44dad60c6f684d87
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-02-02 22:23:07 +0000

    actualizacion

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //import react into the bundle
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 //include bootstrap npm library into the bundle
@@ -10,6 +10,23 @@ import "../styles/index.scss";
 
 //import your own components
 import { Home } from "./component/home.js";
-
+import Navbar from "./component/Navbar.js";
+import Jumbotron from "./component/jumbotron.js";
+import Card from "./component/card.js";
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(
+	<div>
+		<Navbar />
+		<div className="container">
+			<Jumbotron />
+
+			<div class="row">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
+		</div>
+	</div>,
+	document.querySelector("#app")
+);
